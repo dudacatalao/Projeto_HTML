b02b0d0c6619919c3f2dd9d05e72eec953c6f4ad
Laura <laurapigosso@gmail.com>
2023-11-22 16:52:54 -0300

    updates

--- js/dados.js
+++ js/dados.js
@@ -1,67 +1,102 @@
 export const catalogoProdutos = [
     {
         codigoProduto: 1,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 259.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Ferramentas",
+        nomeProduto:"Base e Corretivo Matte",
+        preco: 69.90,
+        imagemProduto:".produtos/maquiagem/prod4/1.png",
+        descricaoProduto:"Bege Claro 2 - 25g",
+        categoria:"Maquiagem",
         mostrarNaHome: false
     },
     {
         codigoProduto: 2,
-        nomeProduto:"Martelo",
-        preco: 359.99,
-        imagemProduto:"./image/ferramentas/martelo.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Ferramentas",
-        mostrarNaHome: true
-        
+        nomeProduto:"Base e Corretivo Matte",
+        preco: 69.90,
+        imagemProduto:".produtos/maquiagem/prod5/Frasco-Transparente-Fosco-Base-Corretivo-Alta-Cobertura-Contorno-Iluminacao-Velvet-Skin-Bege-Medio.png",
+        descricaoProduto:"Amêndoa - 25g",
+        categoria:"Maquiagem",
+        mostrarNaHome: false
     },
     {
         codigoProduto: 3,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 459.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Ferramentas",
+        nomeProduto:"Base e Corretivo Matte ",
+        preco: 69.90,
+        imagemProduto:".produtos/maquiagem/prod6/1.png",
+        descricaoProduto:"Chocolate - 25g",
+        categoria:"Maquiagem",
         mostrarNaHome: false
     },
     {
         codigoProduto: 4,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 459.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Ferramentas",
+        nomeProduto:"Base e Corretivo Matte",
+        preco: 69.90,
+        imagemProduto:".produtos/maquiagem/prod7/1.png",
+        descricaoProduto:"Cacau 2 - 25g",
+        categoria:"Maquiagem",
         mostrarNaHome: false
     },
     {
         codigoProduto: 5,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 559.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Ferramentas",
-        mostrarNaHome: true
+        nomeProduto:"Blush Extra Dimension MAC",
+        preco: 259.90,
+        imagemProduto:".produtos/maquiagem/prod8/1.avif",
+        descricaoProduto:"Rosa amarelado médio - 4g",
+        categoria:"Maquiagem",
+        mostrarNaHome: false
     },
     {
         codigoProduto: 6,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 659.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Ferramentas",
-        mostrarNaHome: true
+        nomeProduto:"Blush Extra Dimension MAC",
+        preco: 259.90,
+        imagemProduto:".produtos/maquiagem/prod9/1.avif",
+        descricaoProduto:"Champanhe rosado claro - 4g",
+        categoria:"Maquiagem",
+        mostrarNaHome: false
     },
     {
         codigoProduto: 7,
-        nomeProduto:"Kit de Brocas e Beats",
-        preco: 759.99,
-        imagemProduto:"./image/ferramentas/kit1.jpg",
-        descricaoProduto:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam, voluptates adipisci est ad natus eius recusandae accusamus delectus corporis consequuntur porro veritatis nesciunt quidem similique voluptate architecto, eum maiores?",
-        categoria:"Home",
-        mostrarNaHome: true
+        nomeProduto:"Sombra Para Olhos Dazzleshadow MAC",
+        preco: 149.00,
+        imagemProduto:".produtos/maquiagem/prod2/1.avif",
+        descricaoProduto:"Bege pêssego com cintilante rosa",
+        categoria:"Maquiagem",
+        mostrarNaHome: false
+    },
+    {
+        codigoProduto: 8,
+        nomeProduto:"Sombra Para Olhos Dazzleshadow MAC",
+        preco: 149.00,
+        imagemProduto:".produtos/maquiagem/prod10/1.avif",
+        descricaoProduto:"Cinza-claro com cintilante prateado",
+        categoria:"Maquiagem",
+        mostrarNaHome: false
+    },
+    {
+        codigoProduto: 9,
+        nomeProduto:"Batom Líquido MAC Locked Kiss INK",
+        preco: 219.00,
+        imagemProduto:".produtos/maquiagem/prod1/1.avif",
+        descricaoProduto:"Matte Até 24 horas - 4ml",
+        categoria:"Maquiagem",
+        mostrarNaHome: false
+    },
+    {
+        codigoProduto: 10,
+        nomeProduto:"Batom Líquido  MAC Powder Kiss",
+        preco: 219.00,
+        imagemProduto:".produtos/maquiagem/prod11/1.avif",
+        descricaoProduto:"Rosa fluorescente brilhante - 5ml",
+        categoria:"Maquiagem",
+        mostrarNaHome: false
+    },
+    {
+        codigoProduto: 11,
+        nomeProduto:"Batom Líquido Retro Matte MAC",
+        preco: 169.90,
+        imagemProduto:".produtos/maquiagem/prod12/1.avif",
+        descricaoProduto:"Batom Líquido Retro Matte MAC",
+        categoria:"Maquiagem",
+        mostrarNaHome: false
     },
     {
         codigoProduto: 8,
