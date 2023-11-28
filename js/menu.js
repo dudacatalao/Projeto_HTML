ae51617092433152f88cfa1afab1cd15a1ef4e5b
dudacatalao <dmelocatalao@gmail.com>
2023-11-28 16:54:47 -0300

    updates

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     
     $('.sub-btn').click(function () {
-        $(this).next('.sub-menu').slideToggle();
+        $(this).next('.sub-menu-adm, .sub-menu').slideToggle();
         $(this).find('.dropdown').toggleClass('rotate');
     });
 
